9babd1ad07b38b77e63e2ac8f5ee6fae0d450361
awa <and3081@yandex.ru>
2022-05-15 21:26:28 +0300

    6 fin

--- src/main/java/Custom/Json/MyJson.java
+++ src/main/java/Custom/Json/MyJson.java
@@ -13,16 +13,11 @@ public class MyJson {
     /**
      * Получение Json-string из List<Map<String, String>>
      * @param list входной List<Map<String, String>>
-     * @return Json-string / null при исключении (trace в консоль)
+     * @return Json-string / null при err
      */
     public static String listMapToJson(List<Map<String, String>> list) {
         JSONArray json = new JSONArray(list);
-        try {
-            return json.toString(2);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return null;
+        return json.toString(2);
     }
 }
 
